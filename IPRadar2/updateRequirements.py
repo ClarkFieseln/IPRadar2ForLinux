c7ad34b5718d5777087048a608ba314e8fd7106e
Clark Fieseln <54716944+ClarkFieseln@users.noreply.github.com>
2026-05-18 17:32:15 +0200

    Update updateRequirements.py

--- IPRadar2/updateRequirements.py
+++ IPRadar2/updateRequirements.py
@@ -1,7 +1,9 @@
 import shlex,  subprocess
 
+# NOTE:
+# Use pip freeze when you want to rebuild the same environment, e.g.:
+#   pip freeze > requirements_venv_as4pgc.txt
+# Use pipreqs when you want to document only direct dependencies from your code.
 print("Updating requirements.txt..")
 p1 = subprocess.Popen(shlex.split("pipreqs --force ./ --ignore backups"), shell=True)
 p1.wait()
-p1.terminate()
-p1.kill()
