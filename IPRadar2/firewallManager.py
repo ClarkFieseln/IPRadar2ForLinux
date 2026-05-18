ec9f79f2be26e3b4cd770b793b757682281cd380
Clark Fieseln <54716944+ClarkFieseln@users.noreply.github.com>
2026-05-18 15:16:25 +0200

    Update firewallManager.py

--- IPRadar2/firewallManager.py
+++ IPRadar2/firewallManager.py
@@ -29,39 +29,29 @@ class FirewallManagerClass(object):
         try:
             currentTime = time.strftime("%Y_%m_%d_%H_%M_%S", time.gmtime())
             # 1) avoid duplicated entries by first trying:
-            logging.info("".join(["Check if firewall rule exists before adding it. IP = " , hostIP]))
-            command = "".join(["ufw status | grep \"" , configuration.RULE_NAME_STR , "\" | grep \"" , hostIP , "\""])
+            logging.info("Check if firewall rule exists before adding it. IP = " +  hostIP)
+            command = "ufw status | grep \"" + configuration.RULE_NAME_STR + "\" | grep \"" + hostIP + "\""
             p1 = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE)
             if p1.returncode == 0:
-                p1.terminate()
-                p1.kill()
                 logging.info("Firewall rule exists already, nothing to do.")
             else:
-                p1.terminate()
-                p1.kill()
                 logging.debug("We can add inbound firewall rule!")
                 #################################################
                 # 2) add rule to block inbound traffic from BAD IP:
-                command = "".join(["ufw deny in comment \"IPRadar2-Block-" , currentTime , ": in from " , self.ruledHostName[hostIP] , "\" from " , hostIP])
+                command = "ufw deny in comment \"IPRadar2-Block-"+currentTime+": in from "+self.ruledHostName[hostIP]+"\" from "+hostIP
                 p2 = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE)
                 p2.wait()
                 if p2.returncode == 0:
-                    p2.terminate()
-                    p2.kill()
                     # 3) add rule to block outbound traffic from BAD IP:
                     logging.debug("We can add outbound firewall rule!")
-                    command = "".join(["ufw deny out comment \"IPRadar2-Block-" , currentTime , ": out to " , self.ruledHostName[hostIP] , "\" to " , hostIP])
+                    command = "ufw deny out comment \"IPRadar2-Block-"+currentTime+": out to "+self.ruledHostName[hostIP]+"\" to "+hostIP
                     p3 = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE)
                     p3.wait()
                     if p3.returncode == 0:
                         logging.info("Added inbound and outbound rules successfully!")
                     else:
                         logging.error("Error: could not add out rule for IP " + hostIP)
-                    p3.terminate()
-                    p3.kill()
                 else:
-                    p2.terminate()
-                    p2.kill()
                     logging.error("Error: could not add in rule for IP " + hostIP)
         except Exception as e:
             logging.exception("Exception in __ruleHost() = " + str(e))
